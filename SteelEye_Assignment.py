15c2258be48751ad86e7e2cae92bed83b1203ab9
Vivekdjango <vivek.unique90@gmail.com>
2018-08-05 23:47:42 +0530

    Python  Assignment

--- SteelEye_Assignment.py
+++ SteelEye_Assignment.py
@@ -9,13 +9,13 @@ from botocore.client import Config
 
 #AWS credentials
 
-ACCESS_KEY_ID = 'AKIAJPQV6RB6YOEXBJTA'
-ACCESS_SECRET_KEY = '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'
+ACCESS_KEY_ID = ''
+ACCESS_SECRET_KEY = ''
 BUCKET_NAME = 'my-vivek-bucket-test1'
 
 #Reading file using Pandas library just replace filepath as per yours
 
-data = pd.read_excel('/home/vvdntech/Documents/Data_Science/Python-Data-Science-and-Machine-Learning-Bootcamp/Python-Data-Science-and-Machine-Learning-Bootcamp/ISO10383_MIC.xls',sheet_name=1)
+data = pd.read_excel('ISO10383_MIC.xls',sheet_name=1)
 
 #To get first five data from the Excel sheet
 print (data.head())
